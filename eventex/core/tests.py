ea0a048f9b963219e5049a03592371c30d49bb44
Henrique <h.braga.albor@gmail.com>
2016-12-12 00:53:29 -0200

    Tests for 404 page.

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -15,3 +15,18 @@ class IndexTest(TestCase):
 
     def test_subscription_link(self):
         self.assertContains(self.resp, 'href="/inscricao/')
+
+
+class InvalidPage(TestCase):
+    """Tests for invalid URL."""
+
+    def setUp(self):
+        self.resp = self.client.get('/not-exists')
+
+    def test_get(self):
+        """Status code must return 404 (Not Found)."""
+        self.assertEqual(404, self.resp.status_code)
+
+    def test_template(self):
+        """Must use template 404.html (Django convention for Not Found)."""
+        self.assertTemplateUsed(self.resp, '404.html')
